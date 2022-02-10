06a773e1a4c4f4a757f835e39bcbae75a654bb13
Jerome <jerome@agilitycms.com>
2022-02-10 11:02:12 -0500

    update: use external postcss addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss"
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        testColor: "#000000",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
