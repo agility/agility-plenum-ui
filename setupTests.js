7f58778c98c59d1cb8817c85073662a43c92e1a7
Jerome <jerome@agilitycms.com>
2022-02-10 14:53:14 -0500

    update: test

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 const Enzyme = require('enzyme');
 // eslint-disable-next-line
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
 Enzyme.configure({ adapter: new Adapter() });
