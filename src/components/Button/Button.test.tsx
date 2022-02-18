f60991fc135dae5e37680a5ead2fd128159c1e36
Jerome <jerome@agilitycms.com>
2022-02-18 16:07:34 -0500

    add: basefield tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,67 +2,67 @@ import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import { Button } from './Button';
 
-describe('Button', function () {
-    describe('Button Size', function () {
+describe('Button', () => {
+    describe('Button Size', () => {
         let button: ShallowWrapper;
         beforeEach(() => {
             button = shallow(<Button label="Primary" type="primary" />);
         });
-        it('should have large font using lg size prop', function () {
+        it('should have large font using lg size prop', () => {
             button.setProps({ size: 'lg' })
             expect(button.hasClass('text-lg')).toBe(true);
         });
-        it('should have base font using base size prop', function () {
+        it('should have base font using base size prop', () => {
             button.setProps({ size: 'base' })
             expect(button.hasClass('text-base')).toBe(true);
         });
-        it('should have small font using sm size prop', function () {
+        it('should have small font using sm size prop', () => {
             button.setProps({ size: 'sm' })
             expect(button.hasClass('text-sm')).toBe(true);
         });
     });
-    describe('Primary Button', function () {
+    describe('Primary Button', () => {
         let button: ShallowWrapper;
         beforeEach(() => {
             button = shallow(<Button label="Primary" type="primary" />);
         });
-        it('should have a label', function () {
+        it('should have a label', () => {
             expect(button.text()).toEqual('Primary');
         });
-        it('should have the correct color class', function () {
+        it('should have the correct color class', () => {
             expect(button.hasClass('bg-purple-600')).toBe(true);
         });
-        it('should have the correct hover color class', function () {
+        it('should have the correct hover color class', () => {
             expect(button.hasClass('hover:bg-purple-700')).toBe(true);
         });
     });
-    describe('Secondary Button', function () {
+    describe('Secondary Button', () => {
         let button: ShallowWrapper;
         beforeEach(() => {
             button = shallow(<Button label="Secondary" type="secondary"/>);
         });
-        it('should have a label', function () {
+        it('should have a label', () => {
             expect(button.text()).toEqual('Secondary');
         });
-        it('should have the correct color class', function () {
+        it('should have the correct color class', () => {
             expect(button.hasClass('bg-purple-100')).toBe(true);
         });
-        it('should have the correct hover color class', function () {
+        it('should have the correct hover color class', () => {
             expect(button.hasClass('hover:bg-purple-200')).toBe(true);
         });
     });
-    describe('Alternative Button', function () {
+    describe('Alternative Button', () => {
         let button: ShallowWrapper;
         beforeEach(() => {
             button = shallow(<Button label="Alternative" type="alternative" />);
         });
-        it('should have a label', function () {
+        it('should have a label', () => {
             expect(button.text()).toEqual('Alternative');
         });
-        it('should have the correct color class', function () {
+        it('should have the correct color class', () => {
             expect(button.hasClass('bg-white')).toBe(true);
         });
-        it('should have the correct hover color class', function () {
+        it('should have the correct hover color class', () => {
             expect(button.hasClass('hover:bg-gray-50')).toBe(true);
         });
     });
--- src/components/Forms/BaseField/BaseField.test.tsx
+++ src/components/Forms/BaseField/BaseField.test.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme'
-import { BaseField } from './BaseField'
+import { shallow } from 'enzyme';
+import { BaseField, Type } from './BaseField';
 
-test('BaseField component', () => {
-    // setup
-    const baseinput = shallow(<BaseField type={"number"} id={""} name={""}  />);
-    // test
-    expect(baseinput.text()).toEqual('');
-})
+describe('BaseField', () => {
+    describe('Type param', () => {
+        const availableTypes = ['text', 'email', 'number', 'password', 'search', 'tel', 'url', 'date', 'datetime-local', 'month', 'time', 'week', 'currency'];
+        it.each(availableTypes)('should have the correct type params', (type) => {
+            const basefield = shallow(<BaseField type={type as Type} id={type} name={type} />);
+            expect(basefield.prop('type')).toBe(type);
+        });
+    });
+    describe('Placeholder', () => {
+        it('should pass the correct placeholder param', () => {
+            const basefield = shallow(<BaseField type='text' name='text' id='text' placeholder="placeholder" />)
+            expect(basefield.prop('placeholder')).toBe('placeholder')
+        });
+    });
+    describe('isDisabled', () => {
+        it('should apply the correct style and prop for the disabled state', () => {
+            const basefield = shallow(<BaseField type='text' name='text' id='text' placeholder="placeholder" isDisabled={true} />)
+            expect(basefield.prop('disabled')).toBe(true);
+            expect(basefield.hasClass('opacity-50')).toBe(true);
+        });
+    });
+});
