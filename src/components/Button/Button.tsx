2b38028cf4f1b38f42eaa21692263b9221855d50
Jerome <jerome@agilitycms.com>
2022-02-22 11:30:40 -0500

    fix: style padding not following styleguide

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -30,11 +30,12 @@ export interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button: FC<ButtonProps> = ({ type = 'primary', size = 'base', onClick, label, icon }: ButtonProps) => {
+
     const btnStyles = cn(
-        'inline-flex space-x-2 items-center justify-center border transition-all min-w-[200px] shadow-sm',
-        {'text-sm': size === 'sm'},
-        {'text-base px-4 py-2 rounded-md': size === 'base'},
-        {'text-lg': size === 'lg'},
+        'inline-flex space-x-2 items-center justify-center border transition-all shadow-sm rounded-md',
+        {'text-sm px-4 py-2': size === 'sm'},
+        {'text-base px-5 py-2': size === 'base'},
+        {'text-lg px-5 py-2': size === 'lg'},
         {'text-white bg-purple-600 hover:bg-purple-700': type === 'primary'},
         {'text-purple-700 bg-purple-100 hover:text-purple-700 hover:bg-purple-200': type === 'secondary'},
         {'text-gray-700 bg-white hover:text-gray-700 hover:bg-gray-50 border-gray-300': type === 'alternative'}
