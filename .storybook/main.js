bf09554070329c4d4a50b1d0f91072835b8c1bd2
Jerome <jerome@agilitycms.com>
2022-02-24 10:56:07 -0500

    add: storybook accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
 module.exports = {
-    stories: ["../src/**/*.stories.tsx"],
+    stories: ['../src/**/*.stories.tsx'],
     addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-a11y',
         {
-            name: "@storybook/addon-postcss",
+            name: '@storybook/addon-postcss',
             options: {
                 postcssLoaderOptions: {
-                    implementation: require("postcss"),
-                },
-            },
-        },
-    ],
+                    implementation: require('postcss')
+                }
+            }
+        }
+    ]
 };
