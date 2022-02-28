434c1f9133350da163b7c6518d16e0339e835368
Jerome <jerome@agilitycms.com>
2022-02-28 16:31:22 -0500

    fix: combobox options z-index is lower than label

--- src/components/Combobox/Combobox.tsx
+++ src/components/Combobox/Combobox.tsx
@@ -65,7 +65,7 @@ export const Combobox: FC<ComboboxProps> = ({
         'absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none'
     );
     const optionStyles = cn(
-        'absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+        'absolute z-30 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
     );
     return (
         <HeadlessUICombobox as="div" value={selectedItem} onChange={setSelectedItem} disabled={isDisabled}>
