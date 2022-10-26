85ed6c777a7c462e3579c9b8e12bf00d809a9066
Dakotah <dakotah.godfrey.dev@gmail.com>
2022-10-26 17:06:39 -0400

    add rended prop for label weight

--- src/components/Forms/Checkbox/Checkbox.tsx
+++ src/components/Forms/Checkbox/Checkbox.tsx
@@ -1,87 +1,100 @@
-import React, { FC } from 'react';
-import { default as cn } from 'classnames';
-import { InputLabel } from '../InputLabel';
-import { useId } from '../../../util/useID';
+import React, { FC } from "react"
+import { default as cn } from "classnames"
+import { InputLabel } from "../InputLabel"
+import { useId } from "../../../util/useID"
 
 export interface CheckboxProps {
-    /** Checkbox label */
-    label: string;
-    /** Checkbox ID */
-    id?: string;
-    /** Disabled state */
-    isDisabled?: boolean;
-    /** value */
-    value?: string;
-    /** Check state */
-    isChecked?: boolean;
-    /** If field is required */
-    isRequired?: boolean;
-    /** Error state */
-    isError?: boolean;
-    /** Message or description */
-    message?: string;
-    /** Callback on input change */
-    onChange?(value: string, isChecked: boolean): void;
+	/** Checkbox label */
+	label: string
+	/** Checkbox ID */
+	id?: string
+	/** Disabled state */
+	isDisabled?: boolean
+	/** value */
+	value?: string
+	/** Check state */
+	isChecked?: boolean
+	/** If field is required */
+	isRequired?: boolean
+	/** Error state */
+	isError?: boolean
+	/** Message or description */
+	message?: string
+	/** Callback on input change */
+	onChange?(value: string, isChecked: boolean): void
+	/** Font weight for the checkbox label*/
+	labelWeight?:
+		| "thin"
+		| "extralight"
+		| "light"
+		| "normal"
+		| "medium"
+		| "semibold"
+		| "bold"
+		| "extrabold"
+		| "black"
 }
 
 /** Comment */
 export const Checkbox: FC<CheckboxProps> = ({
-    label,
-    id,
-    isDisabled,
-    isChecked,
-    isRequired,
-    isError,
-    message,
-    value,
-    onChange,
+	label,
+	id,
+	isDisabled,
+	isChecked,
+	isRequired,
+	isError,
+	message,
+	value,
+	onChange,
+	labelWeight = "medium"
 }: CheckboxProps) => {
-    const uniqueID = useId()
+	const uniqueID = useId()
 	if (!id) id = `cb-${uniqueID}`
 
-    const checboxStyles = cn(
-        'focus:ring-purple-500 h-4 w-4 text-purple-600 border-gray-300 rounded',
-        { 'border-red-500 shadow-none': isError }
-    );
-    const wrapperStyles = cn('relative flex items-start', { 'opacity-50': isDisabled });
+	const checboxStyles = cn(
+		"focus:ring-purple-500 h-4 w-4 text-purple-600 border-gray-300 rounded",
+		{ "border-red-500 shadow-none": isError }
+	)
+	const wrapperStyles = cn("relative flex items-start", {
+		"opacity-50": isDisabled
+	})
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const targetValue = e.currentTarget.value;
-        const targetChecked = e.currentTarget.checked;
-        typeof onChange === 'function' && onChange(targetValue, targetChecked);
-    };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const targetValue = e.currentTarget.value
+		const targetChecked = e.currentTarget.checked
+		typeof onChange === "function" && onChange(targetValue, targetChecked)
+	}
 
-    return (
-        <div className={wrapperStyles}>
-            <div className="flex items-center h-5">
-                <input
-                    id={id}
-                    aria-describedby={`${id}-description`}
-                    name={id}
-                    value={value}
-                    type="checkbox"
-                    className={checboxStyles}
-                    disabled={isDisabled}
-                    checked={isChecked}
-                    onChange={(e) => {
-                        handleChange(e);
-                    }}
-                />
-            </div>
-            <div className="ml-3 text-sm">
-                <label htmlFor={id} className="font-medium text-gray-700">
-                    <InputLabel
-                        label={label}
-                        isRequired={isRequired}
-                        id={id}
-                    />
-                </label>
-                {message && (
-                    <p id={`${id}-description`} className="text-gray-500">
-                        {message}
-                    </p>
-                )}
-            </div>
-        </div>
-    );
-};
+	return (
+		<div className={wrapperStyles}>
+			<div className="flex h-5 items-center">
+				<input
+					id={id}
+					aria-describedby={`${id}-description`}
+					name={id}
+					value={value}
+					type="checkbox"
+					className={checboxStyles}
+					disabled={isDisabled}
+					checked={isChecked}
+					onChange={(e) => {
+						handleChange(e)
+					}}
+				/>
+			</div>
+			<div className="ml-3 text-sm">
+				<label
+					htmlFor={id}
+					className={cn(`font-${labelWeight} text-gray-700`)}
+				>
+					<InputLabel label={label} isRequired={isRequired} id={id} />
+				</label>
+				{message && (
+					<p id={`${id}-description`} className="text-gray-500">
+						{message}
+					</p>
+				)}
+			</div>
+		</div>
+	)
+}
