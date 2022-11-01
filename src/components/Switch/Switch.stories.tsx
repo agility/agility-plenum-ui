9a8912df6a599c651849518fd02cde88478684ac
Dakotah <dakotah.godfrey.dev@gmail.com>
2022-11-01 14:45:38 -0400

    update toggle switch path

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -1,50 +1,55 @@
-import React from 'react';
-import { Meta } from '@storybook/react/types-6-0';
-import { Story } from '@storybook/react';
-import { Switch, SwitchProps } from './Switch';
-import { BRAND_CONFIG } from '../../common';
+import React from "react"
+import { Meta } from "@storybook/react/types-6-0"
+import { Story } from "@storybook/react"
+import { Switch, SwitchProps } from "./Switch"
+import { BRAND_CONFIG } from "../../common"
 
 export default {
-    title: `${BRAND_CONFIG.brandTitle}/Components/Atoms/Switch`,
-    component: Switch
-} as Meta;
+	title: `${BRAND_CONFIG.brandTitle}/Components/Switch`,
+	component: Switch
+} as Meta
 
-const Template: Story<SwitchProps> = (args) => <Switch {...args} />;
+const Template: Story<SwitchProps> = (args) => <Switch {...args} />
 
-
-export const AllVariations = Template.bind({});
+export const AllVariations = Template.bind({})
 AllVariations.decorators = [
-    () => {
-        return (
-            <div className="grid-flow-row gap-4 grid">
-                <Switch {...(SwitchComponentSm.args as SwitchProps)} />
-                <Switch {...(SwitchComponentMd.args as SwitchProps)} />
-                <Switch {...(SwitchComponentLg.args as SwitchProps)} />
-                <span className="text-xs block mt-5 text-gray-400">
-                    Note: controls are disabled on this view
-                </span>
-            </div>
-        );
-    }
-];
+	() => {
+		return (
+			<div className="grid grid-flow-row gap-4">
+				<Switch {...(SwitchComponentSm.args as SwitchProps)} />
+				<Switch {...(SwitchComponentMd.args as SwitchProps)} />
+				<Switch {...(SwitchComponentLg.args as SwitchProps)} />
+				<span className="block mt-5 text-xs text-gray-400">
+					Note: controls are disabled on this view
+				</span>
+			</div>
+		)
+	}
+]
 
-export const SwitchComponentSm = Template.bind({});
+export const SwitchComponentSm = Template.bind({})
 SwitchComponentSm.args = {
-    size: 'sm',
-    onChange: (value:boolean) => {console.log(`onChange ${value}`)},
-};
-SwitchComponentSm.storyName = 'Switch Small';
+	size: "sm",
+	onChange: (value: boolean) => {
+		console.log(`onChange ${value}`)
+	}
+}
+SwitchComponentSm.storyName = "Switch Small"
 
-export const SwitchComponentMd = Template.bind({});
+export const SwitchComponentMd = Template.bind({})
 SwitchComponentMd.args = {
-    size: 'md',
-    onChange: (value:boolean) => {console.log(`onChange ${value}`)},
-};
-SwitchComponentMd.storyName = 'Switch Medium';
+	size: "md",
+	onChange: (value: boolean) => {
+		console.log(`onChange ${value}`)
+	}
+}
+SwitchComponentMd.storyName = "Switch Medium"
 
-export const SwitchComponentLg = Template.bind({});
+export const SwitchComponentLg = Template.bind({})
 SwitchComponentLg.args = {
-    size: 'lg',
-    onChange: (value:boolean) => {console.log(`onChange ${value}`)},
-};
-SwitchComponentLg.storyName = 'Switch Large';
+	size: "lg",
+	onChange: (value: boolean) => {
+		console.log(`onChange ${value}`)
+	}
+}
+SwitchComponentLg.storyName = "Switch Large"
