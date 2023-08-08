ab82d5e76295095971988125426fc309f664a871
Joel Varty <joel@agilitycms.com>
2023-08-08 09:28:42 -0400

    removed @tailwindcss/line-clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -186,7 +186,6 @@ module.exports = {
 	},
 	plugins: [
 		require("@tailwindcss/forms"),
-		require("@tailwindcss/line-clamp"),
 		require("@tailwindcss/typography"),
 		require("@headlessui/tailwindcss")
 	]
