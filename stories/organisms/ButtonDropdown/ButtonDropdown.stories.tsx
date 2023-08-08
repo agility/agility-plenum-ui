9bcdcb4b0b08af88815af813dbbb22d126a2e4ab
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2023-08-08 09:17:55 -0400

    build fix

--- stories/organisms/ButtonDropdown/ButtonDropdown.stories.tsx
+++ stories/organisms/ButtonDropdown/ButtonDropdown.stories.tsx
@@ -1,14 +1,75 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import ButtonDropdown from "."
-import { dropdownDataWithIcons } from "../Dropdown/dropdownItems"
-import { DynamicIcon } from "@/stories/atoms/icons"
+
+import { IItemProp } from "../Dropdown"
 
 const meta: Meta<typeof ButtonDropdown> = {
-	title: "Design System/Organisms/ButtonDropdown",
+	title: "Design System/Organisms/Button Dropdown",
 	component: ButtonDropdown,
 	tags: ["autodocs"],
 	argTypes: {}
 }
+ const dropdownDataWithIcons: IItemProp[][] = [
+		[
+			{
+				icon: {
+					name: "IconClipboardCopy",
+					pos: "leading",
+					className: "h-5 w-5",
+					outline: false
+				},
+				key: "Copy",
+				label: "Copy",
+				onClick: () => {
+					console.log("Copy action")
+				}
+			}
+		],
+		[
+			{
+				icon: {
+					name: "IconFolderPlus",
+					pos: "leading",
+					className: "h-5 w-5",
+					outline: false
+				},
+				key: "Add to Batch",
+				label: "Add to Batch",
+				onClick: () => {
+					console.log("Add to Batch action")
+				}
+			},
+			{
+				icon: {
+					name: "IconEye",
+					pos: "leading",
+					className: "h-5 w-5",
+					outline: false
+				},
+				key: "View Batch",
+				label: "View Batch",
+				onClick: () => {
+					console.log("View Batch action")
+				}
+			}
+		],
+		[
+			{
+				icon: {
+					name: "IconTrash",
+					pos: "leading",
+					className: "h-5 w-5",
+					outline: false
+				},
+				key: "Delete",
+				label: "Delete",
+				onClick: () => {
+					console.log("Delete action")
+				},
+				isEmphasized: true
+			}
+		]
+ ]
 
 export default meta
 type Story = StoryObj<typeof ButtonDropdown>
