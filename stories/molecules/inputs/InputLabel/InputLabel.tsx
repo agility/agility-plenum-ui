9b0f849165257259af451b23f7cd0fe745b1215b
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2024-02-26 11:03:41 -0500

    tweak to cb label

--- stories/molecules/inputs/InputLabel/InputLabel.tsx
+++ stories/molecules/inputs/InputLabel/InputLabel.tsx
@@ -25,7 +25,7 @@ const InputLabel: FC<IInputLabelProps> = ({
 }: IInputLabelProps) => {
 	const labelStyles = cn(
 		"z-[2]",
-		{ "inline-block font-medium transition-all text-sm text-gray-700 mb-1": !isPlaceholder },
+		{ "inline-block font-medium transition-all text-sm text-gray-700 ": !isPlaceholder },
 		{ "inline-block font-medium ml-2 relative transition-all": isPlaceholder },
 		{ "text-sm text-gray-400 px-2 top-8": isPlaceholder && !isActive },
 		{ "text-xs text-gray-700 px-1 top-[10px] bg-white": isPlaceholder && isActive },
