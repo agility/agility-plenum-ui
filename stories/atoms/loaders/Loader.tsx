003c48b48ec19372a8043ca143db659bba26a5fd
DerekAgility <derek@agilitycms.com>
2025-04-11 14:38:10 -0400

    Update to loader

--- stories/atoms/loaders/Loader.tsx
+++ stories/atoms/loaders/Loader.tsx
@@ -1,21 +1,21 @@
-import React from "react"
-import { default as cn } from "classnames"
+import React from "react";
+import { default as cn } from "classnames";
 
 export interface ILoaderProps {
-	className?: string
+	className?: string;
 }
 const Loader: React.FC<ILoaderProps> = ({ className }) => {
 	return (
 		<>
 			<i
 				className={cn(
-					"rounded-full w-16 h-16 inline-block border-8 animate-spin m-auto",
-					className ? className : "border-purple-700 border-r-gray-200"
+					"rounded-full w-16 h-16 inline-block border-4 animate-spinSlower m-auto",
+					className ? className : "border-gray-200 border-r-violet-800"
 				)}
 				role="status"
 			/>
 		</>
-	)
-}
+	);
+};
 
-export default Loader
+export default Loader;
